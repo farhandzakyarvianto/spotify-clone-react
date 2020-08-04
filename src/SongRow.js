d231f6189486ec2a5aa9394c2d67af9bbd8b2bed
farhandzakyarvianto <farhandzaky25@gmail.com>
2020-08-04 17:31:04 +0700

    adding something

--- src/SongRow.js
+++ src/SongRow.js
@@ -8,8 +8,7 @@ function SongRow({ track }) {
             <div className="songRow__info">
                 <h1>{track.name}</h1>
                 <p>
-                    {track.artists.map((artist) => artist.name).join(", ")}
-                    {track.album.name}
+                    {track.artists.map((artist) => artist.name).join(", ")} * {track.album.name}
                 </p>
             </div>
         </div>
